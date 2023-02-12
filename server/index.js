4bc9ddc408f7b3e751d437386599db6a6e90c526
Tulaib <tulluahsid@gmail.com>
2023-02-12 15:05:42 +0500

    2Commit

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ import postRoutes from "./routes/postRoutes.js";
 import dalleRoutes from "./routes/dalleRoutes.js";
 
 dotenv.config();
-
+const port = process.env.PORT || 8080;
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
@@ -21,7 +21,7 @@ app.get("/", async (req, res) => {
 const startServer = async () => {
   try {
     connectDB(process.env.MONGODB_URL);
-    app.listen(8080, () => console.log("server has started on 8080"));
+    app.listen(port, () => console.log("server has started on 8080"));
   } catch (error) {
     console.log(error, "error");
   }
